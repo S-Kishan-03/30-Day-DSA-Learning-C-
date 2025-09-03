26ed67b75ff549ec9c2d3100bfd44f836fb9d79a
S-Kishan-03 <s.kishan9603@gmail.com>
2025-09-03 23:13:51 +0530

    build fix

--- my-vite-react-app/vite.config.ts
+++ my-vite-react-app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/30-Day-DSA-Learning-C-/',
   plugins: [react()],
   server: {
     port: 3000,
